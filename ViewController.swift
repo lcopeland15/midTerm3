9501184789d1aa957c719a63094fcd32c98d6c47
lcopeland15 <59888590+lcopeland15@users.noreply.github.com>
2020-04-08 19:13:04 -0700

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -36,8 +36,33 @@ class ViewController: UIViewController {
     //Stepper
    
     @IBAction func stepperButtonPressed(_ sender: UIStepper) {
+        if sender.tag == 0 {
+            voteCount[0] = Int(sender.value)
+            label0.text = "Warren:  " + String(sender.value)
+        }
+        if sender.tag == 1 {
+            voteCount[1] = Int(sender.value)
+            label1.text = "Sanders:  " + String(sender.value)
+        }
+        if sender.tag == 2 {
+            voteCount[2] = Int(sender.value)
+            label2.text = "Klobuchar:  " + String(sender.value)
+        }
+        if sender.tag == 3 {
+            voteCount[3] = Int(sender.value)
+            label3.text = "Buttigeg:  " + String(sender.value)
+        }
+        if sender.tag == 4 {
+            voteCount[4] = Int(sender.value)
+            label4.text = "Bloomberg:  " + String(sender.value)
+        }
+        if sender.tag == 5 {
+            voteCount[5] = Int(sender.value)
+            label5.text = "Biden:  " + String(sender.value)
+
+            
+        }
     }
-    
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,21 +75,15 @@ class ViewController: UIViewController {
         imageView5.image = UIImage(named: "Biden")
         
     //Labels
-        label0.text = "Warren: \(voteCount[0])"
-        label1.text = "Sanders: \(voteCount[1])"
-        label2.text = "Klobuchar: \(voteCount[2])"
-        label3.text = "Buttigeg: \(voteCount[3])"
-        label4.text = "Bloomberg: \(voteCount[4])"
-        label5.text = "Biden: \(voteCount[5])"
+        label0.text = "Warren: "
+        label1.text = "Sanders: "
+        label2.text = "Klobuchar: "
+        label3.text = "Buttigeg: "
+        label4.text = "Bloomberg: "
+        label5.text = "Biden: "
             
         }
-    //Stepper
-    func stepperButtonPressed(sender: AnyObject) {
-        label0.text = "Warren:\(voteCount[0]) " + String(sender.value)
-        
-        
-    }
-    
+
     
     }
 
@@ -73,5 +92,10 @@ class ViewController: UIViewController {
         
     
   
+
+
+
+
+
 
 
